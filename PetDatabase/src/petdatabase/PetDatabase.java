ae4f3aef01fc37668f040dc1c909a3ac9704f827
Abdisalan <Abdisalan@10.0.0.27>
2023-11-12 16:28:01 -0600

    added error handling

--- PetDatabase/src/petdatabase/PetDatabase.java
+++ PetDatabase/src/petdatabase/PetDatabase.java
@@ -26,11 +26,12 @@ public class PetDatabase {
     public static Scanner scanner = new Scanner(System.in);
     public static int rowCounter = 0;
     public static int idCounter = 0;
-    public static String filename = "pets2.txt";
+    public static String filename = "pets3.txt";
     public static int counter = 0;
     public static String[] SavePetsArray = new String[100];
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         loadPets();//When the program executes, it loads the data from the text file
@@ -43,11 +44,8 @@ public class PetDatabase {
             System.out.println("what would you like to do?");
             System.out.println("1) View all pets");
             System.out.println("2) Add more pets");
-            System.out.println("3) Update an existing pet");
-            System.out.println("4) Remove an existing pet");
-            System.out.println("5) Search pets by name");
-            System.out.println("6) Search pets by age");
-            System.out.println("7) Exit program");
+            System.out.println("3) Delete a pet");
+            System.out.println("4) Exit program");
             System.out.println(" Your choice:");
             int menuChoice = scanner.nextInt();
             switch (menuChoice) {
@@ -59,24 +57,13 @@ public class PetDatabase {
                     addPet();
                     break;
                 case 3:
-                    updatePet();
+                  deletePet();
                     break;
                 case 4:
-                    deletePet();
-                    break;
-                case 5:
-                    searchPetsByName();
-                    break;
-                case 6:
-                    searchPetsByAge();
-                    break;
-                case 7:
-                    savePets();
-                    System.out.println("Data saved. Exiting.");
+                   savePets();
                     System.out.println("Thank you for using pet database");
-             
                     return;
-
+                
             }
         }
     }
@@ -88,45 +75,56 @@ public class PetDatabase {
     }//end of viewAllPets
 
     public static void addPet() {
-        String name;
-        int age;
-        if (rowCounter >= 5) {
+    if (rowCounter >= 5) {
         System.out.println("You've reached the maximum limit of 5 pets. Unable to add more.");
         return; // Exit the method if the limit is reached
     }
-        System.out.println("please enter the pet Information below");
-        scanner.nextLine();
-        while (true) {
-            System.out.print("Enter pet name and age ('Name Age'): ");
-            String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("done")) {
-                break;
-            }
+    String name;
+    int age;
+    System.out.println("Please enter the pet information below");
+    scanner.nextLine();
 
-            String[] parts = input.split(" ");
-            
-            if (parts.length != 2) {//check if they enter more or less than 2 inputs!
-                System.out.println("Invalid input. Please enter name and age separated by a space.");
-                continue;
-            }
+    while (true) {
+        System.out.print("Enter pet name and age ('Name Age'): ");
+        String input = scanner.nextLine();
 
-            name = parts[0];
-
-            try {///checks if they enter a number
-                age = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid age. Please enter a valid integer.");
-                continue;
-            }
-
-            Pet newPet = new Pet(idCounter++, name, age);
-            petsArray.add(newPet);
-            System.out.println("Pet added: " + newPet.getPetName());
-            rowCounter++;
+        if (input.equalsIgnoreCase("done")) {
+            break;
         }
 
-    }//end of addPet
+        String[] parts = input.split(" ");
+
+        if (parts.length != 2) {
+            System.out.println("Invalid input. Please enter name and age separated by a space.");
+            continue;
+        }
+
+        name = parts[0];
+
+        try {
+            age = Integer.parseInt(parts[1]);
+
+            if (age < 1 || age > 20) {
+                System.out.println("Invalid age. Age should be between 1 and 20.");
+                continue; // Ask for input again if age is out of range
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid age. Please enter a valid integer.");
+            continue;
+        }
+
+        Pet newPet = new Pet(idCounter++, name, age);
+        petsArray.add(newPet);
+        System.out.println("Pet added: " + newPet.getPetName());
+        rowCounter++;
+
+        if (rowCounter >= 5) {
+            System.out.println("You've reached the maximum limit of 5 pets. Unable to add more.");
+            break; // Exit the loop if the limit is reached
+        }
+    }
+}
 
     public static void deletePet() {
         System.out.print("Enter the ID of the pet you want to delete(ex:1): ");
@@ -151,80 +149,80 @@ public class PetDatabase {
         System.out.println("Pet with ID " + idToDelete + " not found in the list.");
     }//end of deletePet
 
-    public static void updatePet() {
-        String newName;
-        int newAge;
-        System.out.print("Enter the ID of the pet you want to Update(ex:1): ");
-        int idToUpdate = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter a new name and age for your pet!");
-        String input = scanner.nextLine();
-        String[] parts = input.split(" ");
+//    public static void updatePet() {
+//        String newName;
+//        int newAge;
+//        System.out.print("Enter the ID of the pet you want to Update(ex:1): ");
+//        int idToUpdate = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.println("Enter a new name and age for your pet!");
+//        String input = scanner.nextLine();
+//        String[] parts = input.split(" ");
+//
+//        if (parts.length != 2) {//check if they enter more or less than 2 inputs!
+//            System.out.println("Invalid input. Please enter name and age separated by a space.");
+//
+//        }
+//
+//        newName = parts[0];
+//        newAge = Integer.parseInt(parts[1]);
+//
+//        for (int i = 0; i < petsArray.size(); i++) {
+//            Pet pet = petsArray.get(i);
+//            if (pet.getID() == idToUpdate) {
+//                pet.setPetName(newName);
+//                pet.setAge(newAge);
+//                //System.out.println("Pet with ID " + idToDelete + " has been deleted.");
+//
+//                return;
+//
+//            }
+//
+//        }
+//
+//    }//end of updatePet
 
-        if (parts.length != 2) {//check if they enter more or less than 2 inputs!
-            System.out.println("Invalid input. Please enter name and age separated by a space.");
+//    public static void searchPetsByName() {
+//        Scanner scanner2 = new Scanner(System.in);
+//        int newRowCounter = 0;
+//        System.out.print("Enter the name of the pet you want to find (e.g., roxxy): ");
+//        String nameToSearch = scanner2.nextLine();
+//        System.out.println("Pets with name '" + nameToSearch + "':");
+//        printTableHeader();
+//        for (Pet pet : petsArray) {
+//            if (pet.getPetName().equalsIgnoreCase(nameToSearch)) {
+//
+//                System.out.println("| " + pet.getID() + "|  " + pet.getPetName() + "\t" + "|" + pet.getAge() + " |");
+//                newRowCounter++;
+//            }
+//        }
+//        rowCounter = newRowCounter;
+//        printTableFooter();
+//        rowCounter = petsArray.size();
+//        System.out.println("No pets found with name '" + nameToSearch + "'.");
+//
+//    }//end of search pets by name 
 
-        }
-
-        newName = parts[0];
-        newAge = Integer.parseInt(parts[1]);
-
-        for (int i = 0; i < petsArray.size(); i++) {
-            Pet pet = petsArray.get(i);
-            if (pet.getID() == idToUpdate) {
-                pet.setPetName(newName);
-                pet.setAge(newAge);
-                //System.out.println("Pet with ID " + idToDelete + " has been deleted.");
-
-                return;
-
-            }
-
-        }
-
-    }//end of updatePet
-
-    public static void searchPetsByName() {
-        Scanner scanner2 = new Scanner(System.in);
-        int newRowCounter = 0;
-        System.out.print("Enter the name of the pet you want to find (e.g., roxxy): ");
-        String nameToSearch = scanner2.nextLine();
-        System.out.println("Pets with name '" + nameToSearch + "':");
-        printTableHeader();
-        for (Pet pet : petsArray) {
-            if (pet.getPetName().equalsIgnoreCase(nameToSearch)) {
-
-                System.out.println("| " + pet.getID() + "|  " + pet.getPetName() + "\t" + "|" + pet.getAge() + " |");
-                newRowCounter++;
-            }
-        }
-        rowCounter = newRowCounter;
-        printTableFooter();
-        rowCounter = petsArray.size();
-        System.out.println("No pets found with name '" + nameToSearch + "'.");
-
-    }//end of search pets by name 
-
-    public static void searchPetsByAge() {
-        Scanner scanner2 = new Scanner(System.in);
-        int newRowCounter = 0;
-        System.out.print("Enter the Age of the pet you want to find (e.g.,5): ");
-        int ageToSearch = scanner2.nextInt();
-        System.out.println("Pets with the age of '" + ageToSearch + "':");
-        printTableHeader();
-        for (Pet pet : petsArray) {
-            if (pet.getAge() == ageToSearch) {
-
-                System.out.println("| " + pet.getID() + "|  " + pet.getPetName() + "\t" + "|" + pet.getAge() + " |");
-                newRowCounter++;
-            }
-        }
-        rowCounter = newRowCounter;
-        printTableFooter();
-        rowCounter = petsArray.size();
-        System.out.println("No pets found with the age of '" + ageToSearch + "'.");
-
-    }//end of search pets by age  
+//    public static void searchPetsByAge() {
+//        Scanner scanner2 = new Scanner(System.in);
+//        int newRowCounter = 0;
+//        System.out.print("Enter the Age of the pet you want to find (e.g.,5): ");
+//        int ageToSearch = scanner2.nextInt();
+//        System.out.println("Pets with the age of '" + ageToSearch + "':");
+//        printTableHeader();
+//        for (Pet pet : petsArray) {
+//            if (pet.getAge() == ageToSearch) {
+//
+//                System.out.println("| " + pet.getID() + "|  " + pet.getPetName() + "\t" + "|" + pet.getAge() + " |");
+//                newRowCounter++;
+//            }
+//        }
+//        rowCounter = newRowCounter;
+//        printTableFooter();
+//        rowCounter = petsArray.size();
+//        System.out.println("No pets found with the age of '" + ageToSearch + "'.");
+//
+//    }//end of search pets by age  
 
     public static void printTableHeader() {
         System.out.println("+-------------------+");
